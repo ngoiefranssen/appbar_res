ce2046428dcc311db0d4b7590ff9fefa59aa187d
Franssen <ngoiefranssen04@gmail.com>
2023-01-02 15:00:03 +0200

    Wip

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import React from 'react'
+
+const Header = () => {
+  return (
+    <AppBar>
+      <Toolbar>
+        <Typography>
+        </Typography>
+      </Toolbar> 
+    </AppBar>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+
+rootElement.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
